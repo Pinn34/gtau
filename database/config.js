87df5215b2cf20682892249d40e5235054d7682f
pinnnnnzyyyyy <akunmlprib7900@gmail.com>
2026-04-25 23:05:41 +0700

    Create config.js

---
+++ database/config.js
@@ -0,0 +1,7 @@
+//credit By Limzzzhama 
+//no hapus credit ketauan hapus hitam//
+
+module.exports = {
+  domain: "https://serverku.lynzzofficial.com", // Ubah jadi Domain panel Mu !!!
+  port: "2367" // Ubah Jadi Port Panel Mu !!!
+};
